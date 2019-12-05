7f7827ccb78a20ba516718bc0a11f3b457cd67d1
gsmith660 <gsmith660@gmail.com>
2019-12-04 19:29:20 -0600

    bonus 2 and 3 completed

--- script.js
+++ script.js
@@ -2,16 +2,24 @@
 window.addEventListener("load", function() {
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
         response.json().then(function(json) {
+            let h1 = document.querySelector('h1');
+            h1.innerHTML = `${json.length} ` + h1.innerHTML;
             let htmlToAdd = '';
             for (let i in json) {
                 let data = json[i];
+                let color;
+                if (data.active) {
+                    color = 'green';
+                } else {
+                    color = 'red';
+                }
                 htmlToAdd += `
                 <div class="astronaut">
                     <div class="bio">
                         <h3>${data.firstName} ${data.lastName}</h3>
                         <ul>
                             <li>Hours in space: ${data.hoursInSpace}</li>
-                            <li>Active: ${data.active}</li>
+                            <li style="color: ${color};">Active: ${data.active}</li>
                             <li>Skills: ${data.skills.join(', ')}</li>
                         </ul>
                     </div>
